5050c4250759c09991fbba7b8b1767d3f63552db
wangbuhui <bentoule@qq.com>
2018-07-27 17:40:52 +0800

    Update

---
+++ src/BenDan.ORM/PublicConstant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenDan.ORM
+{
+    public class PublicConstant
+    {
+
+        /// <summary>
+        /// 获取 SQL Server 连接字符串
+        /// </summary>
+        public static string ConnectionString
+        {
+            get
+            {
+                string _connectionString = "";
+                return _connectionString;
+            }
+        }
+
+    }
+}
---
+++ src/BenDan.ORM/SqlServe/SqlDBConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BenDan.ORM.SqlServe
+{
+    public class SqlDBConfig
+    {
+        #region SqlServer 连接配置
+
+        public static IDbConnection GetSqlConnection(string sqlConnectionString = null)
+        {
+            if (string.IsNullOrWhiteSpace(sqlConnectionString))
+            {
+                sqlConnectionString = PublicConstant.ConnectionString;
+            }
+            IDbConnection conn = new SqlConnection(sqlConnectionString);
+            conn.Open();
+            return conn;
+        }
+        #endregion
+    }
+}
